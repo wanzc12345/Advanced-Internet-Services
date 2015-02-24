f005c5e875e7929cefafdad00dd61a8c50b17ae5
Zhicheng Wan <zw2275@columbia.edu>
2015-02-23 20:58:14 -0500

    change name to client

---
+++ RichPresenceClient/RichPresence/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RichPresence
+//
+//  Created by Jensen Wan on 2/22/15.
+//  Copyright (c) 2015 Jensen Wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
