5cdc4b6ba13cf1320ed5c96f340be679babe0564
Zhicheng Wan <zw2275@columbia.edu>
2015-04-02 00:22:03 -0400

    add some new files

---
+++ RichPresenceClient/RichPresence/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  RichPresence
+//
+//  Created by Jensen Wan on 4/1/15.
+//  Copyright (c) 2015 Jensen Wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ RichPresenceClient/RichPresence/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  RichPresence
+//
+//  Created by Jensen Wan on 4/1/15.
+//  Copyright (c) 2015 Jensen Wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UIViewController
+
+@end
