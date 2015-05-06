681e4684297d006f0a9a4994d293ebd2e63131bc
Zhicheng Wan <zw2275@columbia.edu>
2015-05-06 00:23:11 -0400

    hello hello

--- RichPresenceServer/WebContent/pages/index.jsp
+++ RichPresenceServer/WebContent/pages/index.jsp
@@ -365,7 +365,7 @@
                             <div class="row">
                                 <div>
                                     <div class="table-responsive">
-                                        <table  id = "informationtbody" class="table table-bordered table-hover table-striped">
+                                        <table class="table table-bordered table-hover table-striped">
                                             <thead>
                                                 <tr id = "informationhead">
                                                     <th>#</th>
@@ -383,7 +383,7 @@
                                                     <th>betteryLevel</th>
                                                 </tr>
                                             </thead>
-                                            <tbody>
+                                            <tbody id = "informationtbody" >
                                                 
                                             </tbody>
                                         </table>
--- RichPresenceServer/src/GetUserInfo.java
+++ RichPresenceServer/src/GetUserInfo.java
@@ -49,24 +49,27 @@ public class GetUserInfo extends HttpServlet {
 		
 		String query = "SELECT * from User_Info_DB WHERE userID = " + "'" + userID + "'";
 		rs = stmt.executeQuery(query);
-		JSONObject userInfo = new JSONObject();
+		JSONArray userInfo = new JSONArray();
 		try {
 		  while(rs.next()) {
+			JSONObject a = new JSONObject();
 			String password = rs.getString("password");
 			String firstName  = rs.getString("firstName");
 			String lastName = rs.getString("lastName");
 			String homeAddress = rs.getString("homeAddress");
 			String workAddress = rs.getString("workAddress");
-			userInfo.put("password", password);
-			userInfo.put("firstName", firstName);
-			userInfo.put("lastName", lastName);
-			userInfo.put("homeAddress", homeAddress);
-			userInfo.put("workAddress", workAddress);
+			a.put("password", password);
+			a.put("firstName", firstName);
+			a.put("lastName", lastName);
+			a.put("homeAddress", homeAddress);
+			a.put("workAddress", workAddress);
+			userInfo.add(a);
 		  }
 		  stmt1 = conn.createStatement();
 		  String query1 = "SELECT * from Rich_Presence_DB WHERE userID = " + "'" + userID + "'";
 		  rs1 = stmt1.executeQuery(query1);
 		  while (rs1.next()){
+			  JSONObject a = new JSONObject();
 			  String timestamp = rs1.getString("timestamp");
 			  String latitude = rs1.getString("latitude");
 			  String longitude = rs1.getString("longitude");
@@ -79,18 +82,19 @@ public class GetUserInfo extends HttpServlet {
 			  String OSType = rs1.getString("OSType");
 			  String OSVersion = rs1.getString("OSVersion");
 			  String serialNumber = rs1.getString("serialNumber");
-			  userInfo.put("timestamp", timestamp);
-			  userInfo.put("latitude", latitude);
-			  userInfo.put("longitude", longitude);
-			  userInfo.put("xAcc", xAcc);
-			  userInfo.put("yAcc", yAcc);
-			  userInfo.put("zAcc", zAcc);
-			  userInfo.put("volume", volume);
-			  userInfo.put("brightness", brightness);
-			  userInfo.put("batteryLevel", batteryLevel);
-			  userInfo.put("OSType", OSType);
-			  userInfo.put("OSVersion", OSVersion);
-			  userInfo.put("serialNumber", serialNumber);
+			  a.put("timestamp", timestamp);
+			  a.put("latitude", latitude);
+			  a.put("longitude", longitude);
+			  a.put("xAcc", xAcc);
+			  a.put("yAcc", yAcc);
+			  a.put("zAcc", zAcc);
+			  a.put("volume", volume);
+			  a.put("brightness", brightness);
+			  a.put("batteryLevel", batteryLevel);
+			  a.put("OSType", OSType);
+			  a.put("OSVersion", OSVersion);
+			  a.put("serialNumber", serialNumber);
+			  userInfo.add(a);
 		  }
 		  response.getWriter().write(userInfo.toString());
 		} catch (Exception e) {
